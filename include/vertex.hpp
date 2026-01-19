f6a10c7150062876a7137002bc12afdbb02584f7
matty <mattreed1890@gmail.com>
2026-01-19 15:47:28 -0600

    present window

---
+++ include/vertex.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+struct Vertex {
+  glm::vec3 pos, norm;
+  glm::vec2 tex;
+};
---
+++ src/game.cpp
@@ -0,0 +1,53 @@
+#include <game.hpp>
+#include <glad/glad.h>
+#include <SDL3/SDL.h>
+#include <stdexcept>
+
+SDL_Window *window;
+SDL_GLContext context;
+constexpr int WIDTH = 1920, HEIGHT = 1080;
+
+void init() {
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+  window = SDL_CreateWindow("drivin", WIDTH, HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+  if (window == NULL) {
+    // In the case that the window could not be made...
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+    std::runtime_error("could not create SDL window");
+  }
+
+  context = SDL_GL_CreateContext(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+    std::runtime_error("could not load glad");
+  }
+}
+
+void run() {
+  bool done = false;
+  while (!done) {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+          done = true;
+      }
+    }
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    SDL_GL_SwapWindow(window);
+  }
+}
+
+void cleanup() {
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
